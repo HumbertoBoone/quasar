60d29a558d016358d20f7f6df31d336186a74be3
Humberto Boone Abdo <h.boone@outlook.com>
2022-04-26 15:57:45 -0500

    Add function description , remove unnecessary ar

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.json())
+
+// CORS 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'codepen.io', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
